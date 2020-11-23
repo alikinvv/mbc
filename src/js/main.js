47de760d44f74e147832f243f6a94d641d45affa
Valery Alikin <weblcw@gmail.com>
2020-11-23 20:24:50 +0500

    update

---
+++ src/js/main.js
@@ -0,0 +1,65 @@
+let main = new Swiper('.main__slider', {
+    loop: true,
+    navigation: {
+        nextEl: '.main__slider .swiper-button-next',
+        prevEl: '.main__slider .swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+let reviews = new Swiper('.reviews__slider', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.reviews__slider .swiper-button-next',
+        prevEl: '.reviews__slider .swiper-button-prev',
+    },
+});
+
+let find = new Swiper('.find__list', {
+    init: false,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loopFillGroupWithBlank: true,
+});
+
+$('input[name="phone"]').mask('+7 (000) 000-00-00');
+
+let changeView = () => {
+    if ($(window).width() <= 1023 && $('.hamburger').length === 0) {
+        $('.header__main .container').append('<div class="hamburger"><span></span></div>');
+
+        find = new Swiper('.find__list', {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            loopFillGroupWithBlank: true,
+        });
+    } else if ($(window).width() > 1023  && $('.hamburger').length > 0) {
+        $('.hamburger').remove();
+        find.destroy();
+    }
+}
+
+if ($(window).width() <= 1023) {
+    changeView();
+    find.init();
+}
+
+$(window).on('resize', changeView);
+
+$('body').on('click', '.hamburger', (e) => {
+    $(e.currentTarget).toggleClass('active');
+    $('.header__menu').toggleClass('active');
+    $('html, body').toggleClass('overflow');
+});
+
+$('body').on('click', '.close-menu', () => {
+    $('.hamburger').toggleClass('active');
+    $('.header__menu').toggleClass('active');
+});
