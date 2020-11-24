83e5f704c4a58258385c0df9c47201426734b275
Valery Alikin <weblcw@gmail.com>
2020-11-24 18:24:49 +0500

    update

--- build/js/main.js
+++ build/js/main.js
@@ -22,9 +22,9 @@ let reviews = new Swiper('.reviews__slider', {
     },
     breakpoints: {
         768: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         }
-      }
+    }
 });
 
 let find = new Swiper('.find__list', {
@@ -32,26 +32,93 @@ let find = new Swiper('.find__list', {
     slidesPerView: 2,
     spaceBetween: 30,
     loopFillGroupWithBlank: true,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        320: {
+            slidesPerView: 1.4,
+            spaceBetween: 20,
+        }
+    }
 });
 
+let caseSlider = new Swiper('.case__list', { init: false });
+let service = new Swiper('.service__list', { init: false });
+
 $('input[name="phone"]').mask('+7 (000) 000-00-00');
+
+let sameHeight = (elem) => {
+    let highestBox = 0;
+
+    $(elem).each(function() {
+        if ($(this).height() > highestBox) {
+            highestBox = $(this).height(); 
+        }
+    });  
+
+    $(elem).removeClass('height');
+    $(elem).height(highestBox);
+}
+
+sameHeight('.service__item');
 
 let changeView = () => {
     if ($(window).width() <= 1023 && $('.hamburger').length === 0) {
         $('.header__main .container').append('<div class="hamburger"><span></span></div>');
+        $('.header__menu').prepend('<img src="img/logo.png" alt="">');
 
         find = new Swiper('.find__list', {
             slidesPerView: 2,
             spaceBetween: 30,
             loopFillGroupWithBlank: true,
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                },
+                320: {
+                    slidesPerView: 1.4,
+                    spaceBetween: 20,
+                }
+            }
         });
 
         $('.footer').append('<div class="container mobile"></div>');
         $('.footer .mobile').append($('.footer p'));
         $('.footer .mobile').append($('.footer__social'));
+
+        if ($(window).width() <= 767) {
+            $('.marketolog').each((el, i) => {
+                $(i).find('.marketolog__online').before($(i).find($('.marketolog__statusbar')));
+            });
+
+            caseSlider = new Swiper('.case__list', {
+                slidesPerView: 1.5,
+                spaceBetween: 16,
+                loopFillGroupWithBlank: true,
+            });
+
+            service = new Swiper('.service__list', {
+                slidesPerView: 1.2,
+                spaceBetween: 18,
+                loopFillGroupWithBlank: true,
+            });
+        } else {
+            $('.marketolog').each((el, i) => {
+                $(i).find('.marketolog__info-top').append($(i).find($('.marketolog__statusbar')));
+            });
+
+            caseSlider.destroy();
+            service.destroy();
+        }
     } else if ($(window).width() > 1023  && $('.hamburger').length > 0) {
         $('.hamburger').remove();
+        $('.header__menu img').remove();
         find.destroy();
+        caseSlider.destroy();
+        service.destroy();
         $('.footer .container:not(.mobile) .footer__col:last').append($('.footer .container.mobile p'));
         $('.footer .container:not(.mobile) .footer__col:last').append($('.footer .container.mobile .footer__social'));
         $('.footer .container.mobile').remove();
