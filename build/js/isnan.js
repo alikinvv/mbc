9f58e5a5adb0f7206853163c4bafb10426bedb2e
valikin <valikin@knoema.com>
2020-12-18 21:08:30 +0500

    update

--- build/js/isnan.js
+++ build/js/isnan.js
@@ -1,32 +1,30 @@
-"use strict";
-
 if (!Number.isNaN) {
-  (function (global) {
-    var defineProperty = function () {
-      // IE 8 only supports `Object.defineProperty` on DOM elements
-      try {
-        var object = {};
-        var $defineProperty = Object.defineProperty;
-        var result = $defineProperty(object, object, object) && $defineProperty;
-      } catch (error) {}
+	(function(global) {
+		var defineProperty = (function() {
+			// IE 8 only supports `Object.defineProperty` on DOM elements
+			try {
+				var object = {};
+				var $defineProperty = Object.defineProperty;
+				var result = $defineProperty(object, object, object) && $defineProperty;
+			} catch(error) {}
+			return result;
+		}());
 
-      return result;
-    }();
+		var globalIsNaN = global.isNaN;
 
-    var globalIsNaN = global.isNaN; // Source: http://wiki.ecmascript.org/doku.php?id=harmony:number.isnan
+		// Source: http://wiki.ecmascript.org/doku.php?id=harmony:number.isnan
+		var isNaN = function(value) {
+			return typeof value === 'number' && globalIsNaN(value);
+		};
 
-    var isNaN = function isNaN(value) {
-      return typeof value === 'number' && globalIsNaN(value);
-    };
-
-    if (defineProperty) {
-      defineProperty(Number, 'isNaN', {
-        'value': isNaN,
-        'configurable': true,
-        'writable': true
-      });
-    } else {
-      Number.isNaN = isNaN;
-    }
-  })(void 0);
+		if (defineProperty) {
+			defineProperty(Number, 'isNaN', {
+				'value': isNaN,
+				'configurable': true,
+				'writable': true
+			});
+		} else {
+			Number.isNaN = isNaN;
+		}
+	}(this));
 }
